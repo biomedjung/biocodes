b5a04201283eb09f38e94a17faef20366c04c6d8
Guilherme Jung <27732278+biomedjung@users.noreply.github.com>
2021-12-30 07:51:51 -0300

    Add files via upload

---
+++ equ.py
@@ -0,0 +1,12 @@
+# Código escrito com o propósito de automatizar cáculos no Exame Qualitativo de Urina(EQU).
+
+campo1 = input()
+campo2 = input()
+campo3 = input()
+campo4 = input()
+campo5 = input()
+campo6 = input()
+campo7 = input()
+campo8 = input()
+campo9 = input()
+campo10 = input()
